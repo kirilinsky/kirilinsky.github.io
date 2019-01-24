55d7555dd770948b10bf513071d2288a7fc27a6d
kirill <kirill.ilinsky@gmail.com>
2019-01-24 17:48:32 +0300

    k

--- ajax/ip.php
+++ ajax/ip.php
@@ -1,10 +1,9 @@
 <?php 
 
 	$name = $_POST['name'];
-	$score = $_POST['score'];
-	$money = $_POST['money'];
+	$phone = $_POST['score'];
 	$dt = date('Y-M-d');
 
-	file_put_contents('scores.txt', "$dt name: $name, score: $score, money: $money \r", FILE_APPEND);
-	echo 'good';
+	file_put_contents('clients.txt', "$dt - клиент: $name, оставлял заявку, его номер: $phone \r", FILE_APPEND);
+	mail("dudinov_d@mail.ru", "Обратная свзяь от $name" , "Добрый день, нам написал $name. \n Он ждет звонка по номеру: $phone" , "From: admin@plitochnik-rnd.ru \r\n")
 
