cacf16c6db6dd86fbc9c2d39f3435b8945cda226
kirill <kirill.ilinsky@gmail.com>
2019-02-11 18:27:14 +0300

    a

--- ajax/ip.php
+++ ajax/ip.php
@@ -1,8 +1,14 @@
 <?php 
 
 	$name = $_POST['name'];
-	$score = $_POST['score'];
-	$money = $_POST['money'];
+	$number = $_POST['number'];
+	$email = $_POST['email'];
+	$name = htmlspecialchars($name);
+	$number = htmlspecialchars($number);
+	$email = htmlspecialchars($email);
+	$name = urldecode($name);
+	$number = urldecode($number);
+	$email = urldecode($email);
 	$dt = date('Y-M-d');
 
 	file_put_contents('scores.txt', "$dt name: $name, score: $score, money: $money \r", FILE_APPEND);
