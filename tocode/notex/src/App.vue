02433d997ded93e83f31ce060c0a9e2f983de417
kirill <kirill.ilinsky@gmail.com>
2019-04-17 17:13:26 +0300

    take with beautify

--- tocode/notex/src/App.vue
+++ tocode/notex/src/App.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="wrap" id="app">
     <!-- newNote -->
-    <newNote 
-      :note="note"
-      :active="active"
-      @addNote="addNote"
-    ></newNote>
+    <newNote :note="note" :active="active" @addNote="addNote"></newNote>
     <!-- end newNote -->
     <navbar @changeLayout="changeLayout"></navbar>
     <box :notes="notes" :listLayout="listLayout"></box>
@@ -18,42 +14,47 @@ import newNote from '@/components/NewNote.vue'
 import navbar from '@/components/Navbar.vue'
 import box from '@/components/Notes.vue'
 export default {
-  components:{
-    newNote,navbar,box
+  components: {
+    newNote,
+    navbar,
+    box
   },
   data() {
     return {
       note: {
         title: '',
         text: '',
-        edit:false,
-        emodji:'2'
+        edit: false,
+        emodji: '2'
       },
-      notes: [
-        {
+      notes: [{
           title: "Hello ",
           text: "Im fuckin best procrastinator",
-          emodji:'1',
-          edit:false,
+          emodji: '1',
+          edit: false,
           dt: moment('2019-04-11 11:07').format('LLL')
         },
         {
           title: "B-b!",
           text: "See ya later",
-          emodji:'3',
-          edit:false,
+          emodji: '3',
+          edit: false,
           dt: moment('2019-04-14 21:23').format('LLL')
         }
       ],
       listLayout: false,
-      active:false
+      active: false
     };
   },
   methods: {
     addNote() {
       this.active = true;
       if (!this.errorsCheck) {
-        let { title, text,emodji } = this.note;
+        let {
+          title,
+          text,
+          emodji
+        } = this.note;
         this.notes.push({
           title,
           text,
@@ -67,30 +68,31 @@ export default {
         this.notice = ''
       }
     },
-    changeLayout(bool){
-      if(bool){
+    changeLayout(bool) {
+      if (bool) {
         this.listLayout = true
-      }  if(!bool) {
+      }
+      if (!bool) {
         this.listLayout = false
       }
     }
-
   },
-  computed:{
+  computed: {
     errorsCheck() {
-      if (this.note.title == ""){
+      if (this.note.title == "") {
         //отдает ошибку о пустом заголовке
         this.notice = 'empty title'
         return true;
-      } if(this.note.text == "") {
-         //отдвет ошибку об отсутствии текста
-         this.notice = 'empty text'
+      }
+      if (this.note.text == "") {
+        //отдвет ошибку об отсутствии текста
+        this.notice = 'empty text'
         return true;
       } else {
-         this.notice = ''
-         return false;
+        this.notice = ''
+        return false;
       }
     }
   }
-};
+}
 </script>
