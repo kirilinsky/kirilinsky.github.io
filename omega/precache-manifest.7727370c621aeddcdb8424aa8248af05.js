4e9b8fc2c56435fc2d4aaf9da0d304f5ea91c512
Kir <37263320+kirilinsky@users.noreply.github.com>
2019-11-21 10:36:01 +0300

    Add files via upload

---
+++ omega/precache-manifest.7727370c621aeddcdb8424aa8248af05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3614ecdfda5aee9e7342668b6fb195ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "894000bcb4027e18ccaa",
+    "url": "/static/css/main.2b69dcce.chunk.css"
+  },
+  {
+    "revision": "e3d0497f09e837bef42f",
+    "url": "/static/js/2.06362f30.chunk.js"
+  },
+  {
+    "revision": "894000bcb4027e18ccaa",
+    "url": "/static/js/main.e86dcc06.chunk.js"
+  },
+  {
+    "revision": "aea3fd21bb46bbefe2db",
+    "url": "/static/js/runtime-main.c69c57be.js"
+  }
+]);
