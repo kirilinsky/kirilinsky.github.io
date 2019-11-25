599268522ccdb5c8a667fa9ad75358ef67fc56ec
Kir <37263320+kirilinsky@users.noreply.github.com>
2019-11-25 17:28:20 +0300

    add init

---
+++ redesk/precache-manifest.000a52b85225fa7f40a387da7c2243e6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e18db5c32749b4bdb6df29233b63093",
+    "url": "/redesk/index.html"
+  },
+  {
+    "revision": "4fde49f6460ee9cf208d",
+    "url": "/redesk/static/css/2.3a988bc3.chunk.css"
+  },
+  {
+    "revision": "74df7f14a80f8ffeef30",
+    "url": "/redesk/static/css/main.0d99b5f7.chunk.css"
+  },
+  {
+    "revision": "4fde49f6460ee9cf208d",
+    "url": "/redesk/static/js/2.fd55bc23.chunk.js"
+  },
+  {
+    "revision": "74df7f14a80f8ffeef30",
+    "url": "/redesk/static/js/main.be64bc11.chunk.js"
+  },
+  {
+    "revision": "0cb0febc3c43af12f2d4",
+    "url": "/redesk/static/js/runtime-main.bd0afa20.js"
+  },
+  {
+    "revision": "a8838083d36c995ead1154d572cc3925",
+    "url": "/redesk/static/media/full1.a8838083.png"
+  },
+  {
+    "revision": "e709e786cd80e080dbae86fe314c1707",
+    "url": "/redesk/static/media/full2.e709e786.png"
+  },
+  {
+    "revision": "7911ba0cd640d2d90e2cfe19d0e9159e",
+    "url": "/redesk/static/media/full3.7911ba0c.png"
+  },
+  {
+    "revision": "bb8a1786f1870664e97ec77f40e0a797",
+    "url": "/redesk/static/media/full4.bb8a1786.png"
+  },
+  {
+    "revision": "d31aea42d1f45f181840ddeb8f1df849",
+    "url": "/redesk/static/media/meme.d31aea42.png"
+  },
+  {
+    "revision": "5a1d3d3d34dcbffddf567a3a78f1b210",
+    "url": "/redesk/static/media/sample1.5a1d3d3d.png"
+  },
+  {
+    "revision": "bd16539c486bff405ff3032dd2beda21",
+    "url": "/redesk/static/media/sample2.bd16539c.png"
+  },
+  {
+    "revision": "dc5390891a9b6596f0f642e73848d968",
+    "url": "/redesk/static/media/sample3.dc539089.png"
+  }
+]);
