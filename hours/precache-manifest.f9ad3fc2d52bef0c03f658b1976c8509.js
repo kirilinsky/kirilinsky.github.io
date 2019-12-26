4034b58bc79b96347a36b550ee1074e87a80a1b5
Kir <37263320+kirilinsky@users.noreply.github.com>
2019-12-26 19:05:59 +0300

    Add files via upload

---
+++ hours/precache-manifest.f9ad3fc2d52bef0c03f658b1976c8509.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89911b8079965fbf5aaff9075e4cd8ef",
+    "url": "/hours/index.html"
+  },
+  {
+    "revision": "bb04f0baa0172f31542f",
+    "url": "/hours/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "bfb7795a625f20b339aa",
+    "url": "/hours/static/css/main.3b1598f9.chunk.css"
+  },
+  {
+    "revision": "bb04f0baa0172f31542f",
+    "url": "/hours/static/js/2.f3d19408.chunk.js"
+  },
+  {
+    "revision": "744b76f766044157687ca8b61b891873",
+    "url": "/hours/static/js/2.f3d19408.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bfb7795a625f20b339aa",
+    "url": "/hours/static/js/main.9ed82173.chunk.js"
+  },
+  {
+    "revision": "8b6b012658ddbcacb4ac",
+    "url": "/hours/static/js/runtime-main.28f4a034.js"
+  },
+  {
+    "revision": "9731a3a162f1a011424f5eb20a1384b4",
+    "url": "/hours/static/media/mainError.9731a3a1.jpg"
+  }
+]);
